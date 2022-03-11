64c77e5fce4ba0e377b17feda23af9d79eeed53a
Hatim Lagzar <LHatimDev@gmail.com>
2022-03-11 16:58:37 +0100

    Website model and migration

---
+++ app/Models/Website.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Website extends Model
+{
+    use HasFactory;
+
+    public const ID_COLUMN = 'id';
+    public const URL_COLUMN = 'url';
+    public const CREATED_AT_COLUMN = 'created_at';
+    public const UPDATED_AT_COLUMN = 'updated_at';
+}
